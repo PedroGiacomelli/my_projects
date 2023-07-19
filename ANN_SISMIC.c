845d4f726c39c98162d9911620c4b9895f7d566f
PedroGiacomelli <pedro.giacomelli2@gmail.com>
2023-07-19 14:37:00 -0300

    v0.5
    In this update, the test of the forward propagation function was done.
    Some mistakes were found during the correction and are alrefy fixed.
    Also, adaptations were done to enable MSP430F5529 reading the file and
    running it. The code was also tested in MSP430, and positive results were seen.
    The next step is creating a function to classify the sample according
    to the results given by the network.

--- ANN_SISMIC.c
+++ ANN_SISMIC.c
@@ -1,22 +1,24 @@
+// Importing msp430 library
 #include <msp430.h>
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+*/
 
 #define N_INPUTS 8
 #define N_HIDDEN_LAYERS 2
 #define LEN_HIDDEN_LAYERS 15
 
 void network_initializer(long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], char n_inputs, char n_hidden_layers, char len_hidden_layers);
-//int layers_creator(char x, unsigned int *hidden_weights);
-//void print_hidden_layers(char n_hidden_layers, char len_hidden_layers);
 void forward_propagation(long final_vals[LEN_HIDDEN_LAYERS], long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], unsigned int *inputs, char n_inputs, char n_hidden_layers, char len_hidden_layers, unsigned int *activation_vec);
 long ReLu(long val);
 char output_result(long *result, char len_hidden_layers);
 
-//long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS];
-
 int main() {
+
+    // Remember stopping watchdog timer. Otherwise the program will not run properly.
+    WDTCTL = WDTPW | WDTHOLD;
 
     char n_inputs=N_INPUTS;
     char n_hidden_layers=N_HIDDEN_LAYERS;
@@ -29,70 +31,105 @@ int main() {
     network_initializer(hidden_layers, n_inputs, n_hidden_layers, len_hidden_layers);
 
     char i, j;
+
+    //Throughout the program, certain sections were commented out to enable MSP430 reading it.
+    // Configuring it to accept printf was unnecessary since using an IDE like CCS already provides the necessary functionality for printing.
+
+    /*
     for (i = 0; i < n_hidden_layers; i++) {
-        for (j = 0; j < len_hidden_layers+1; j++) {
+        printf("\n-0-0-0-0-\n");
+        printf("%d ", i);
+        printf("\n-0-0-0-0-\n");
+        for (j = 0; j < len_hidden_layers; j++) {
             printf("%d ", hidden_layers[i][j]);
         }
         printf("\n");
     }
+    */
 
-    //print_hidden_layers(n_hidden_layers, len_hidden_layers);
     forward_propagation(final_vals,hidden_layers, inputs, n_inputs, n_hidden_layers, len_hidden_layers, activation_vec);
 
-    //output_result(result);
+    /*
+
+    printf("0000000000\n");
+
+    for (i = 0; i < len_hidden_layers; i++) {
+
+        printf("%d ", i);
+        printf("%d ", hidden_layers[N_HIDDEN_LAYERS-1][i]);
+        printf("\n");
+
+    }
+
+    */
+
+   // Loop to allow analysing the result in MSP430
+    while(1);
 
     return 0;
 }
 
 void network_initializer(long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], char n_inputs, char n_hidden_layers, char len_hidden_layers) {
-    //long hidden_layers[N_HIDDEN_LAYERS];
-    // hidden_layers = (unsigned int **)malloc(n_hidden_layers * sizeof(unsigned int *));
     char i,j;
-    //long temp_layer[LEN_HIDDEN_LAYERS]; //={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
     for (i = 0; i < n_hidden_layers; i++) {
         for (j=0; j<LEN_HIDDEN_LAYERS; j++){
-            //hidden_layers[i] = temp_layer[LEN_HIDDEN_LAYERS];
-            // hidden_layers[i] = (unsigned int *)malloc((len_hidden_layers+1) * sizeof(unsigned int));
 
-            hidden_layers[i][j]=i*j;
+            hidden_layers[i][j]=(i+1)*j;
+
         }
     }
 }
 
-void forward_propagation(long final_vals[LEN_HIDDEN_LAYERS], long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], unsigned int *inputs, char n_inputs, char n_hidden_layers, char len_hidden_layers, unsigned int *activation_vec){
+void forward_propagation(long final_vals[LEN_HIDDEN_LAYERS], long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], unsigned int *inputs, char n_inputs, char n_hidden_layers, char len_hidden_layers, unsigned int activation_vec[N_HIDDEN_LAYERS]){
 
 char i,j;
 long activation;
-//long new_output[LEN_HIDDEN_LAYERS];
 
-
-for (i=0; i<n_inputs; i++){
+for (j=0; j<len_hidden_layers; j++){
     activation=activation_vec[0];
 
-    for (j=0; j<len_hidden_layers; j++){
+    for (i=0; i<n_inputs; i++){
 
-        activation+= inputs[i]* hidden_layers[0][j];
+        activation+=inputs[i]*hidden_layers[0][j];
 
     }
 
-    final_vals[i]=ReLu(activation);
+    final_vals[j]=ReLu(activation);
 
 }
 
+/*
+
+for (i = 0; i < len_hidden_layers; i++) {
+
+        printf("%d ", i);
+        printf("%d ", final_vals[i]);
+        printf("\n");
+
+    }
+
+    printf("0000000000\n");
+
+
+*/
+
 char index_hidden_layers;
 
-for (index_hidden_layers=0; index_hidden_layers<n_hidden_layers; index_hidden_layers++){
+for (index_hidden_layers=0; index_hidden_layers<n_hidden_layers-1; index_hidden_layers++){
 
     for (i=0; i<len_hidden_layers; i++){
 
         activation=activation_vec[index_hidden_layers+1];
 
         for (j=0; j<len_hidden_layers; j++){
-            activation+= final_vals[i]*hidden_layers[index_hidden_layers+1][j];
+
+
+
+            activation+= final_vals[j]*hidden_layers[index_hidden_layers+1][i];
         }
 
-        final_vals[i]=ReLu(activation);
+        hidden_layers[index_hidden_layers+1][i]=ReLu(activation);
 
     }
 
@@ -118,8 +155,6 @@ char output_result(long *result, char len_hidden_layers){
             max_info[i]=i;
         }
     }
-
-    //define here the function to find the class.
 
     char test=0;
     return test;
