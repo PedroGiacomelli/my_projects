f18d5a7a3b9408f90630f980d3bae357ef853af9
Pedro Giacomelli <pedro.giacomelli2@gmail.com>
2023-07-26 08:25:00 -0300

    v0.7
    In this version a big fix in the code was done regarding the structure
    of the ANN (the multiplication between the layers was wrong). Moreover,
    the problem of overflow in the previous version was overcome because
    in simulations done, it was seen that on worst case scenarios for the input, the
    overflow did not occurred because the negative weights helped decreasing
    the values of the sums. Also, if necessary, more allocation of
    memory can be done (meaning that the layers can assume long
    long data type) to solve the overflow issue and still have empty space to use.
    In addition, the weights and biases were converted from float to char
    to maintain low-usage of memory. However, it still can be converted to
    int type witout problems regarding memory mapping.

--- ANN_SISMIC.c
+++ ANN_SISMIC.c
@@ -1,68 +1,108 @@
 // Importing msp430 library
-#include <msp430.h>
-/*
+//#include <msp430.h>
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-*/
 
-#define N_INPUTS 8
+
+#define N_INPUTS 11
+#define N_OUTPUTS 1
 #define N_HIDDEN_LAYERS 2
 #define LEN_HIDDEN_LAYERS 15
 
-void network_initializer(long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], char n_inputs, char n_hidden_layers, char len_hidden_layers);
-void forward_propagation(long temp_vals[LEN_HIDDEN_LAYERS], long final_vals[LEN_HIDDEN_LAYERS], long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], unsigned int *inputs, char n_inputs, char n_hidden_layers, char len_hidden_layers, unsigned int *activation_vec);
+void forward_propagation(char input_weights[N_INPUTS][LEN_HIDDEN_LAYERS], char hidden_weights[LEN_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], char output_weights[LEN_HIDDEN_LAYERS],
+                        char input_bias[LEN_HIDDEN_LAYERS], char hidden_bias[LEN_HIDDEN_LAYERS], char output_bias[N_OUTPUTS],
+                        long long temp_vals[LEN_HIDDEN_LAYERS],long long final_vals[LEN_HIDDEN_LAYERS],
+                        unsigned int *inputs);
+
 long ReLu(long val);
-char output_result(long *result, char len_hidden_layers);
 
 int main() {
 
     // Remember stopping watchdog timer. Otherwise the program will not run properly.
-    WDTCTL = WDTPW | WDTHOLD;
+    //WDTCTL = WDTPW | WDTHOLD;
 
-    char n_inputs=N_INPUTS;
-    char n_hidden_layers=N_HIDDEN_LAYERS;
-    char len_hidden_layers=LEN_HIDDEN_LAYERS;
-    unsigned int inputs[N_INPUTS]={1023, 1023, 1023, 1023, 1023, 1023, 1023, 1023}; // Worst case scenario projected
-    unsigned int activation_vec[N_HIDDEN_LAYERS]={1023, 1023}; // Worst case scenario projected
-    long temp_vals[LEN_HIDDEN_LAYERS];
-    long final_vals[LEN_HIDDEN_LAYERS];
-    long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS];
+    long long temp_vals[LEN_HIDDEN_LAYERS], final_vals[LEN_HIDDEN_LAYERS];
 
-    network_initializer(hidden_layers, n_inputs, n_hidden_layers, len_hidden_layers);
+    //unsigned int inputs[N_INPUTS]={251, 2024, 2007, 2161, 2172, 2214, 2207, 2163, 2113, 2112, 2241};
+    unsigned int inputs[N_INPUTS]={4097, 4097, 4097, 4097, 4097, 4097, 4097, 4097,40973, 4097, 4097};
+
+    char input_weights[N_INPUTS][LEN_HIDDEN_LAYERS]={
+        {-79,  21, -75, -53, -35,  73,  33,   8, -96,  48, -21,
+            62, -49, -91,  75},
+        {-57, -21, -38, -87,  70,  71,  96, -24,  93, -11,  35,
+            -85,  83, -41, -49},
+        {-101,    8,   -5,   28,   98,   84,   83,    5,  -81,
+            -65,   93,  -18,   77,   35,   26},
+        {-46,  80, -60, -20, 101,  48, -12,  12, -18,  46, -21,
+            35,  44,  22,   8},
+        {-60, -63,  61, -43,  32, -41, -74, -20, -39, -53,  18,
+            -52,  53,  45,  40},
+        { -81,   89,    1,   82,  -62,   19,   94,  102,  -97,
+                -4,  -43,  -89,   16, -101,   23},
+        { 76,  76,  93,  49, -65, -13,  77, -50, -12,  24, -81,
+            -2, -89, -39,  51},
+        { 44,  89,  57, -58,  83,  11,  70,  86,  67, -22,  20,
+            -14,  42,  -3, -76},
+        {  30,  -62,   50,  -81, -101,  -34,   95,   41,  -18,
+            -78,  -90,   46,   85,  -99,   17},
+        { 47, -43,   5,  64,  20, -25, -87,  -3,  -7, -65, -88,
+                2, -33, -58, -83},
+        {  69,  -31,   67,  -97, -102,   87,   35,  101,   -4,
+            -70,   93,  -68,  -24,   89,  -86}};
+
+    char hidden_weights[LEN_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS]={
+        { 90,  31, -78,  17, -55,  56, -29,   0,  39, -63,  19,
+            -65,  62, -32,   9},
+        { 64,  91, -33, -78,  88,  93,  77,  67,  -1,  -6,  11,
+            -74,  75,  19,  93},
+        { 38, -26, -62, -86, -33, -27,  -7,  30, -79, -39, -57,
+            30,  14,  32, -54},
+        {-91, -88,  -8,  72,   7,   5,   8,   3,  74, -17,  61,
+            63,  18, -57,  52},
+        {-94, -85,  15, -47, -45, -87,  76,  11, -91,  14,  39,
+            41, -10, -14, -43},
+        { 86,  57,  94,  78, -18, -12,  19, -33, -89, -64,  10,
+                8,  88,   3,  31},
+        {-41, -16, -82, -11, -57, -83,  48, -68,  75, -60,  58,
+            74, -96,  17,  58},
+        { 83,  18,  54,   1,  15, -50, -17,  29, -41, -12,  84,
+            -74, -20,  53, -76},
+        { 10, -28,   5, -33, -81,  75,  92, -14,  39, -15,  50,
+            57, -37, -87,  94},
+        { 39,  42,  29, -83, -85,  -2,  58,  91,  16, -77, -29,
+            84,  30, -13,  88},
+        { 49, -64, -68,  30,  91,  59,  41,  51, -64,  -2,  21,
+            -59, -85, -45,  74},
+        {-87, -40, -77,  34, -47,  -1, -39,  73,  42, -58,  23,
+            -70, -39, -26, -17},
+        { 83, -85,  59,  24,  61, -45, -93, -80, -80, -63, -62,
+            37,  32, -90,  12},
+        { 82,  14, -13, -67,  26, -22, -38, -73, -38, -63, -29,
+            -59,  31,  62,  49},
+        {-15, -48,  13,  76,  46,  37, -24, -62, -63,  94, -64,
+            65,  53,  12,  79}};
+
+    char output_weights[LEN_HIDDEN_LAYERS]={-43, -62, 24, -50, 38, 56, 97, -47, 113, 127, 13, -37, 120, 76, 12};
+
+    char input_bias[LEN_HIDDEN_LAYERS]={-9, 79, -37, 95, 102, -3, -37, 97, 102, -11, -93, -11, 43, 26, 35};
+    char hidden_bias[LEN_HIDDEN_LAYERS]={79, 67, -65, -33, -2, -20, -76, 66, -40, -35, 53, -30, 60, -73, 52};
+    char output_bias[N_OUTPUTS]={47};
 
     char i, j;
 
-    //Throughout the program, certain sections were commented out to enable MSP430 reading it.
-    // Configuring it to accept printf was unnecessary since using an IDE like CCS already provides the necessary functionality for printing.
 
-    /*
-    for (i = 0; i < n_hidden_layers; i++) {
-        printf("\n-0-0-0-0-\n");
-        printf("%d ", i);
-        printf("\n-0-0-0-0-\n");
-        for (j = 0; j < len_hidden_layers; j++) {
-            printf("%d ", hidden_layers[i][j]);
-        }
+    forward_propagation(input_weights,  hidden_weights, output_weights, input_bias, hidden_bias, output_bias, temp_vals, final_vals, inputs);
+
+
+    for (i = 0; i < LEN_HIDDEN_LAYERS; i++)
+    {
         printf("\n");
+        printf("%lld ", temp_vals[i]);
     }
-    */
-
-    forward_propagation(temp_vals, final_vals,hidden_layers, inputs, n_inputs, n_hidden_layers, len_hidden_layers, activation_vec);
-
-    /*
-
-    printf("0000000000\n");
-
-    for (i = 0; i < len_hidden_layers; i++) {
-
-        printf("%d ", i);
-        printf("%d ", hidden_layers[N_HIDDEN_LAYERS-1][i]);
-        printf("\n");
-
-    }
-
-    */
+    
 
    // Loop to allow analysing the result in MSP430
     while(1);
@@ -70,19 +110,74 @@ int main() {
     return 0;
 }
 
+void forward_propagation(char input_weights[N_INPUTS][LEN_HIDDEN_LAYERS], char hidden_weights[LEN_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], char output_weights[LEN_HIDDEN_LAYERS],
+                        char input_bias[LEN_HIDDEN_LAYERS], char hidden_bias[LEN_HIDDEN_LAYERS], char output_bias[N_OUTPUTS],
+                        long long temp_vals[LEN_HIDDEN_LAYERS], long long final_vals[LEN_HIDDEN_LAYERS],
+                        unsigned int *inputs)
+                        {
 
-void network_initializer(long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], char n_inputs, char n_hidden_layers, char len_hidden_layers) {
-    char i,j;
 
-    for (i = 0; i < n_hidden_layers; i++) {
-        for (j=0; j<LEN_HIDDEN_LAYERS; j++){
+long long activation=0;
+char i,j=0;
 
-            hidden_layers[i][j]=128; //Worst case scenario projected
+for (i=0; i<LEN_HIDDEN_LAYERS; i++){
 
-        }
+    activation= input_bias[i];
+
+    for (j=0; j<N_INPUTS; j++) {
+
+        activation+=input_weights[j][i]*inputs[j];
+
+    }
+
+    temp_vals[i]=ReLu(activation);
+
+}
+
+
+for (i=0; i<LEN_HIDDEN_LAYERS; i++){
+
+    activation= hidden_bias[i];
+
+    for (j=0; j<LEN_HIDDEN_LAYERS; j++){
+
+        activation+= hidden_weights[i][j]*temp_vals[j];
+
+    }
+
+    final_vals[i]=ReLu(activation);
+
+}
+
+for(i=0; i < LEN_HIDDEN_LAYERS; i++){
+
+    activation=output_bias[0];
+
+    for (j=0; j < LEN_HIDDEN_LAYERS; j++){
+
+        activation+=output_weights[i]*final_vals[j];
+
+    }
+
+    temp_vals[i]=ReLu(activation);
+
+}
+
+char max_val=0;
+
+for (i; i<LEN_HIDDEN_LAYERS; i++){
+
+    if (temp_vals[i]>max_val){
+        max_val=temp_vals[i];
     }
 }
 
+
+
+}
+
+
+/*
 
 void forward_propagation(long temp_vals[LEN_HIDDEN_LAYERS], long final_vals[LEN_HIDDEN_LAYERS], long hidden_layers[N_HIDDEN_LAYERS][LEN_HIDDEN_LAYERS], unsigned int *inputs, char n_inputs, char n_hidden_layers, char len_hidden_layers, unsigned int activation_vec[N_HIDDEN_LAYERS]){
 
@@ -90,6 +185,7 @@ char i,j;
 long activation;
 
 for (j=0; j<len_hidden_layers; j++){
+
     activation=activation_vec[0];
 
     for (i=0; i<n_inputs; i++){
@@ -102,7 +198,7 @@ for (j=0; j<len_hidden_layers; j++){
 
 }
 
-/*
+
 
 for (i = 0; i < len_hidden_layers; i++) {
 
@@ -115,7 +211,7 @@ for (i = 0; i < len_hidden_layers; i++) {
     printf("0000000000\n");
 
 
-*/
+
 
 char index_hidden_layers;
 
@@ -140,6 +236,8 @@ for (index_hidden_layers=0; index_hidden_layers<n_hidden_layers-1; index_hidden_
 
 }
 
+*/
+
 long ReLu(long val){
     if (val<0){
         return 0;
@@ -147,18 +245,3 @@ long ReLu(long val){
     return val;
 }
 
-char output_result(long *result, char len_hidden_layers){
-
-    char i;
-    long max_info[2]={0,0};
-
-    for (i=0; i<len_hidden_layers; i++){
-        if (result[i]> max_info[0]){
-            max_info[0]=result[0];
-            max_info[i]=i;
-        }
-    }
-
-    char test=0;
-    return test;
-}
